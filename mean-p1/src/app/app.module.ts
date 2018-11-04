34c02e19abe4a712417118604a32b3d0570bd2c8
Ankit Vartak <ankitvartak@Ankits-MacBook-Pro.local>
2018-11-04 17:36:47 -0500

    Added post-list component

--- mean-p1/src/app/app.module.ts
+++ mean-p1/src/app/app.module.ts
@@ -4,13 +4,15 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule} from '@angular/material';
+import {MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule} from '@angular/material';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    PostListComponent,
     HeaderComponent
   ],
   imports: [
@@ -20,7 +22,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ mean-p1/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: 'post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent implements OnInit {
+  constructor() { }
+  posts = [
+    // {title: 'First Post', content: 'Data of first'},
+    // {title: '2nd Post', content: 'Data of 2nd'},
+    // {title: '3rd Post', content: 'Data of 3rd'},
+  ];
+
+  ngOnInit() { }
+}
