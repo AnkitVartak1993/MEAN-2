4a119badbb2bbb5b6ac028afb4b77f33c95a8b6a
Ankit Vartak <ankitvartak@Ankits-MacBook-Pro.local>
2018-11-04 20:34:07 -0500

    Added Post Model

--- mean-p1/src/app/posts/post-create/post-create.component.ts
+++ mean-p1/src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Button } from 'protractor';
-
+import {Post} from './../post.model';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -10,11 +10,11 @@ import { Button } from 'protractor';
 export class PostCreateComponent {
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
   newPost = 'No content';
 
   onAddPost() {
-    const post = {
+    const post: Post = {
       title: this.enteredTitle,
       content: this.enteredContent
     };
