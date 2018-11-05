b1b9dd0d896489dbf7ba46db906e5077ae73ed1f
Ankit Vartak <ankitvartak@Ankits-MacBook-Pro.local>
2018-11-05 15:59:39 -0500

    connected to mongoDB atlas cloud

--- mean-p1/backend/app.js
+++ mean-p1/backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Post = require('./models/post');
+const mongoose = require('mongoose');
+const config = require('./../config');
 
 const app = express();
+mongoose.connect(config.mongodb_connString)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch(() => {
+    console.log('MongoDB connection failed');
+  });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -10,13 +21,19 @@ app.use((req,res, next) => {
   res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods','GET, POST, DELETE, PATCH');
   next();
-})
-app.use((req, res, next) => {
-  next();
 });
 
+// app.use((req, res, next) => {
+//   next();
+// });
+
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
+
   console.log(post);
   res.status(201).json({
     message: 'Received successfully.'
